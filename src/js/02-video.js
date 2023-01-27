0c99bc61906afb534080db0b88ffe6defb48c06f
DOPinIT <111194712+DOPinIT@users.noreply.github.com>
2023-01-27 20:42:33 +0200

    work_task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,26 @@
 import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
 const iFrame = document.getElementById("vimeo-player")
 
 const player = new Player(iFrame);
-  
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+const onPlay = 
+    player.on("timeupdate", function sec(sec) {
+     player.getCurrentTime(sec).then(function (seconds) {
+         console.log(seconds, "current seconds ");
+        });
+    });
+
+
 iFrame.addEventListener("click", onPlay)
 
-function onPlay({target}) {
-console.log(target);
-    return target.currentTime.value;
-};
+
