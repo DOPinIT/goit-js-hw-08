12f4aa6f279f5eb150ab13ccf8cb5f1dc53258ac
DOPinIT <111194712+DOPinIT@users.noreply.github.com>
2023-01-27 17:40:47 +0200

    task1_done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from "@vimeo/player";
 
+const iFrame = document.getElementById("vimeo-player")
+
+const player = new Player(iFrame);
+  
+iFrame.addEventListener("click", onPlay)
+
+function onPlay({target}) {
+console.log(target);
+    return target.currentTime.value;
+};
